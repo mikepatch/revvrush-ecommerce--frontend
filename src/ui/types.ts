c350cc96f7f5ac4e23062402bbde0bd7bbc70966
Michal Lata <michallata.dev@gmail.com>
2024-02-07 19:07:12 +0100

    feat: Add ProductItemType

---
+++ src/ui/types.ts
@@ -0,0 +1,15 @@
+export type ProductItemType = {
+	id: string;
+	category: string;
+	name: string;
+	price: number;
+	coverImage: {
+		src: string;
+		alt: string;
+	};
+	bulletPoints?: {
+		label: string;
+		value: string;
+		id: string;
+	}[];
+};
