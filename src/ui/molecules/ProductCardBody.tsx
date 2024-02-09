367f5e418fc6d82da2a6b58d5d5af1d320c6ab83
Michal Lata <michallata.dev@gmail.com>
2024-02-09 10:41:00 +0100

    feat: Add ProductCardBody component

---
+++ src/ui/molecules/ProductCardBody.tsx
@@ -0,0 +1,24 @@
+import { ProductCardPrice } from "@/ui/atoms/ProductCardPrice";
+import { ProductCardBulletPointList } from "@/ui/molecules/ProductCardBulletPointList";
+
+type ProductCardBodyProps = {
+	product: {
+		bulletPoints?: {
+			id: string;
+			label: string;
+			value: string;
+		}[];
+		suitableFor?: string[];
+		category: string;
+		price: number;
+	};
+};
+
+export const ProductCardBody = ({ product }: ProductCardBodyProps) => {
+	return (
+		<article className="flex flex-wrap gap-4 px-4 pb-4">
+			<ProductCardBulletPointList product={product} />
+			<ProductCardPrice price={product.price} />
+		</article>
+	);
+};
