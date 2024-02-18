51b06a0127c866c7dbb3ac5692d9dedb7e50caaf
Michal Lata <michallata.dev@gmail.com>
2024-02-18 09:17:37 +0100

    chore: Add tailwindcss/typography plugin in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/typography")],
 };
 // eslint-disable-next-line import/no-default-export
 export default config;
