5277d282381c46115cd4efbc67d606dc6cb0d3d3
Michal Lata <michallata.dev@gmail.com>
2024-02-18 09:17:11 +0100

    chore: Add typedRoutes and mdxRs experimental features. Add temporary remote hostname for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withMDX from "@next/mdx";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	pageExtensions: ["ts", "tsx", "mdx"],
+	experimental: {
+		typedRoutes: true,
+		mdxRs: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				hostname: "naszsklep-api.vercel.app",
+			},
+		],
+	},
+};
+
+export default withMDX()(nextConfig);
