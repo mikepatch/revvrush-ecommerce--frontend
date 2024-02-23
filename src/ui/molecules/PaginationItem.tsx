3d6a8f3d982076ad3c0afb01b5f1213564adb1da
Michal Lata <michallata.dev@gmail.com>
2024-02-23 19:31:04 +0100

    feat: Add PaginationItem component

--- src/ui/molecules/PaginationItem.tsx
+++ src/ui/molecules/PaginationItem.tsx
@@ -13,7 +13,7 @@ export const PaginationItem = ({ pageNumber, label, ariaLabel = "" }: Pagination
 		<li className="flex items-center border-t-2 border-slate-300 hover:border-brand-primary">
 			<ActiveLink
 				href={`/products/${pageNumber}` as Route}
-				className="flex px-4 pt-4 leading-none transition-all  hover:text-brand-primary"
+				className="flex px-2 pt-2 leading-none transition-all hover:text-brand-primary sm:px-4 sm:pt-4"
 				activeClassName="-mt-[2px] border-t-2 border-brand-primary text-brand-primary"
 				ariaLabel={ariaLabel}
 			>
