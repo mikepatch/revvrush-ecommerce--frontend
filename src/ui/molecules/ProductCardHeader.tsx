6b5417be1572e8dadc608c513db394f81db9f771
Michal Lata <michallata.dev@gmail.com>
2024-02-27 16:08:44 +0100

    patch: Update ProductCardHeader component

--- src/ui/molecules/ProductCardHeader.tsx
+++ src/ui/molecules/ProductCardHeader.tsx
@@ -1,8 +1,9 @@
-import { ProductCardImage } from "@/ui/atoms/ProductCardImage";
+import { ProductImage } from "@/ui/atoms/ProductImage";
 import { ProductCardTitle } from "@/ui/atoms/ProductCardTitle";
 
 type ProductCardHeaderProps = {
 	product: {
+		stock: number;
 		coverImage: {
 			src: string;
 			alt: string;
@@ -14,7 +15,14 @@ type ProductCardHeaderProps = {
 export const ProductCardHeader = ({ product }: ProductCardHeaderProps) => {
 	return (
 		<header className="flex grow flex-col border-b border-slate-300 pb-2">
-			<ProductCardImage src={product.coverImage.src} alt={product.coverImage.alt} />
+			<div className="relative">
+				{product.stock === 0 && (
+					<div className="absolute inset-0 z-10 flex items-center justify-center bg-white bg-opacity-80">
+						<p className="text-xl font-bold">Out of stock</p>
+					</div>
+				)}
+				<ProductImage src={product.coverImage.src} alt={product.coverImage.alt} resizeOnHover />
+			</div>
 			<ProductCardTitle title={product.name} />
 		</header>
 	);
