b83ffdfd81b2b828adc8e2e0c1dc537b3736908f
Michal Lata <michallata.dev@gmail.com>
2024-02-27 16:08:20 +0100

    feat: Add ProductImage component

---
+++ src/ui/atoms/ProductImage.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import NextImage from "next/image";
+
+type ProductImageProps = {
+	src: string;
+	alt: string;
+	width?: number;
+	height?: number;
+	resizeOnHover?: boolean;
+};
+
+export const ProductImage = ({
+	src,
+	alt,
+	width = 300,
+	height = 300,
+	resizeOnHover = false,
+}: ProductImageProps) => {
+	return (
+		<figure className="w-full grow-0 overflow-hidden p-4">
+			<NextImage
+				width={width}
+				height={height}
+				src={src}
+				alt={alt}
+				className={clsx(
+					"aspect-square h-full w-full object-cover object-center transition-all",
+					resizeOnHover && "group-hover:scale-105",
+				)}
+				loading="lazy"
+			/>
+		</figure>
+	);
+};
