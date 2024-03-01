e1da75fcc43928b3c893e6104b5c08e72ed6850b
Michal Lata <michallata.dev@gmail.com>
2024-03-01 20:02:55 +0100

    patch: Update LayoutWithNavigation layout

--- src/app/(with-navigation)/layout.tsx
+++ src/app/(with-navigation)/layout.tsx
@@ -1,13 +1,22 @@
+import { getAllCategories } from "@/api/products";
 import { Header } from "@/ui/organisms/Header";
+import { type NavItemType } from "@/ui/types";
 
 type LayoutWithNavigationProps = {
 	children: React.ReactNode;
 };
 
-export default function LayoutWithNavigation({ children }: LayoutWithNavigationProps) {
+export default async function LayoutWithNavigation({ children }: LayoutWithNavigationProps) {
+	const { productCategories } = await getAllCategories();
+	const categoriesItems = productCategories.data.map((category) => ({
+		id: category.id,
+		href: `/categories/${category.slug}`,
+		label: category.name,
+	})) as NavItemType[];
+
 	return (
 		<>
-			<Header />
+			<Header categories={categoriesItems} />
 			<main className="mx-auto max-w-[1440px] p-6">{children}</main>
 		</>
 	);
