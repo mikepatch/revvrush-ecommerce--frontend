43e6163d632fc1f3ee7438e731e09c9cd41a4858
Michal Lata <michallata.dev@gmail.com>
2024-03-05 20:57:39 +0100

    patch: Update PaginationItem component

--- src/ui/molecules/PaginationItem.tsx
+++ src/ui/molecules/PaginationItem.tsx
@@ -5,14 +5,20 @@ import { ActiveLink } from "@/ui/atoms/ActiveLink";
 type PaginationItemProps = {
 	pageNumber: number;
 	label: string | React.ReactNode;
+	categorySlug: string;
 	ariaLabel?: string;
 };
 
-export const PaginationItem = ({ pageNumber, label, ariaLabel = "" }: PaginationItemProps) => {
+export const PaginationItem = ({
+	pageNumber,
+	label,
+	categorySlug,
+	ariaLabel = "",
+}: PaginationItemProps) => {
 	return (
 		<li className="flex items-center border-t-2 border-slate-300 hover:border-brand-primary">
 			<ActiveLink
-				href={`/products/${pageNumber}` as Route}
+				href={`/categories/${categorySlug}/${pageNumber}` as Route}
 				className="flex px-2 pt-2 leading-none transition-all hover:text-brand-primary sm:px-4 sm:pt-4"
 				activeClassName="-mt-[2px] border-t-2 border-brand-primary text-brand-primary"
 				ariaLabel={ariaLabel}
