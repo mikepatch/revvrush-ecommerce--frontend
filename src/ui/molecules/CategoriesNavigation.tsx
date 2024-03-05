0353fe7f446e03f6c908786aadcc5e8532d465d7
Michal Lata <michallata.dev@gmail.com>
2024-03-05 20:57:09 +0100

    patch: Update CategoriesNavigation component

--- src/ui/molecules/CategoriesNavigation.tsx
+++ src/ui/molecules/CategoriesNavigation.tsx
@@ -1,13 +1,28 @@
-import { NavList } from "@/ui/molecules/NavList";
+import { ActiveLink } from "@/ui/atoms/ActiveLink";
 import { type NavItemType } from "@/ui/types";
 
-export const CategoriesNavigation = ({ categories }: { categories: NavItemType[] }) => {
+export const CategoriesNavigation = ({
+	categories,
+	className,
+}: {
+	categories: NavItemType[];
+	className: React.ComponentProps<"nav">["className"];
+}) => {
 	return (
-		<nav className="flex w-full">
-			<NavList
-				navItems={categories}
-				className="flex w-full justify-center gap-4 bg-font-light py-2 text-font-dark"
-			/>
+		<nav className={`${className} sm:h-10`}>
+			<ul className="flex h-full w-full flex-col justify-center bg-font-light px-4 text-font-dark sm:flex-row">
+				{categories.map(({ id, href, label }) => (
+					<li key={id} className="h-full">
+						<ActiveLink
+							href={href}
+							className="flex h-full items-center border-brand-primary px-2 py-1 transition-all hover:border-b-2"
+							activeClassName="border-b-2"
+						>
+							{label}
+						</ActiveLink>
+					</li>
+				))}
+			</ul>
 		</nav>
 	);
 };
