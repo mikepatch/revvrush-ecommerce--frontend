4bff059f57547a0380245e8fe2c584afb32c585c
Michal Lata <michallata.dev@gmail.com>
2024-03-08 18:39:22 +0100

    patch: Update ProductList component

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -10,11 +10,11 @@ type ProductListProps = {
 export const ProductList = ({ products }: ProductListProps) => {
 	return (
 		<ul
-			className="grid grid-cols-1 justify-center justify-items-center gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+			className="grid grid-cols-1 justify-center justify-items-center gap-4 sm:grid-cols-3 xl:grid-cols-4"
 			data-testid="products-list"
 		>
 			{products.map((product) => (
-				<li key={product.id} className="group w-full max-w-[400px]">
+				<li key={product.id} className="group w-full max-w-[350px]">
 					<Link href={`/product/${product.id}`}>
 						<ProductCard product={product} />
 					</Link>
