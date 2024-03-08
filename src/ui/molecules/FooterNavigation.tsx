029ab334f167862dc7303b355089e4b334a697f9
Michal Lata <michallata.dev@gmail.com>
2024-03-08 18:40:26 +0100

    feat: Add FooterNavigation component

--- src/ui/molecules/FooterNavigation.tsx
+++ src/ui/molecules/FooterNavigation.tsx
@@ -1,12 +1,24 @@
+import { ActiveLink } from "@/ui/atoms/ActiveLink";
 import { Heading } from "@/ui/atoms/Heading";
 import { FOOTER_LEGAL_NAV_ITEMS } from "@/ui/constants";
-import { NavList } from "@/ui/molecules/NavList";
 
 export const FooterNavigation = () => {
 	return (
 		<nav>
 			<Heading level={2} className="text-xl font-bold" text="Legal" />
-			<NavList navItems={FOOTER_LEGAL_NAV_ITEMS} className="flex flex-col gap-2" />
+			<ul className="flex flex-col gap-2">
+				{FOOTER_LEGAL_NAV_ITEMS.map(({ id, href, label }) => (
+					<li key={id}>
+						<ActiveLink
+							href={href}
+							className="border-brand-primary py-1 transition-all hover:border-b-2"
+							activeClassName="border-b-2"
+						>
+							{label}
+						</ActiveLink>
+					</li>
+				))}
+			</ul>
 		</nav>
 	);
 };
