531034f262326d1516b00fe57ba11080ec90755c
Michal Lata <michallata.dev@gmail.com>
2024-03-08 18:40:38 +0100

    feat: Add CategoriesNavigationComponent

--- src/ui/molecules/CategoriesNavigation.tsx
+++ src/ui/molecules/CategoriesNavigation.tsx
@@ -10,12 +10,12 @@ export const CategoriesNavigation = ({
 }) => {
 	return (
 		<nav className={`${className} sm:h-10`}>
-			<ul className="flex h-full w-full flex-col justify-center bg-font-light px-4 text-font-dark sm:flex-row">
+			<ul className="flex h-full w-full flex-col justify-center bg-font-light px-4 py-4 text-font-dark sm:flex-row sm:py-0">
 				{categories.map(({ id, href, label }) => (
 					<li key={id} className="h-full">
 						<ActiveLink
 							href={href}
-							className="flex h-full items-center border-brand-primary px-2 py-1 transition-all hover:border-b-2"
+							className="flex h-full items-center border-brand-primary py-1 transition-all hover:border-b-2 sm:px-2"
 							activeClassName="border-b-2"
 						>
 							{label}
