17a1bb3fc67d749d2c7ae464e5a9d416db60e063
Michal Lata <michallata.dev@gmail.com>
2024-03-12 17:45:36 +0100

    patch: Remove unnecessary type

--- src/ui/types.ts
+++ src/ui/types.ts
@@ -1,24 +1,8 @@
 import { type Route } from "next";
 
-export type ProductItemType = {
-	id: string;
-	category: string;
-	name: string;
-	price: number;
-	coverImage: {
-		src: string;
-		alt: string;
-	};
-	bulletPoints?: {
-		label: string;
-		value: string;
-		id: string;
-	}[];
-	suitableFor?: string[];
-};
-
 export type NavItemType = {
 	id: string;
 	href: Route;
 	label: string;
+	icon?: React.ReactNode;
 };
