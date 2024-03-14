b76460db077b46d4fd284c9afbff46baa7d45341
Michal Lata <michallata.dev@gmail.com>
2024-03-14 18:49:23 +0100

    patch: Temporary disable feature in ProductCardHeader

--- src/ui/molecules/ProductCardHeader.tsx
+++ src/ui/molecules/ProductCardHeader.tsx
@@ -10,11 +10,11 @@ export const ProductCardHeader = ({ product }: ProductCardHeaderProps) => {
 	return (
 		<header className="flex grow flex-col border-b border-slate-300 pb-2">
 			<div className="relative">
-				{product.stock === 0 && (
+				{/* {product.stock === 0 && (
 					<div className="absolute inset-0 z-10 flex items-center justify-center bg-white bg-opacity-80">
 						<p className="text-xl font-bold">Out of stock</p>
 					</div>
-				)}
+				)} */}
 				{product.images && product.images[0] && (
 					<ProductImage src={product.images[0]} alt={product.name} resizeOnHover />
 				)}
