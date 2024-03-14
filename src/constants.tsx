5f2e3fcce5353615a8c2f481b6e44ff5d1c74ec0
Michal Lata <michallata.dev@gmail.com>
2024-03-14 18:42:23 +0100

    patch: Move constants file

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,6 +1,9 @@
 import { type Route } from "next";
 import { ShoppingBasket, User } from "lucide-react";
-import { type NavItemType } from "@/ui/types";
+
+import { type NavItemType } from "@/types";
+
+export const PRODUCTS_ON_PAGE = 4;
 
 export const FOOTER_LEGAL_NAV_ITEMS: NavItemType[] = [
 	{ id: "privacy-policy", href: "/static/privacy-policy" as Route, label: "Privacy Policy" },
