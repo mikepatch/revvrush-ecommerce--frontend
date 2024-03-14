bd66b363647c0af42e4211648a32dd945e989ce4
Michal Lata <michallata.dev@gmail.com>
2024-03-14 18:33:14 +0100

    feat: Add convertSlugToTitle helper

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,10 @@ export const formatPrice = (amount: number): string => {
 		minimumFractionDigits: 2,
 	}).format(amount);
 };
+
+export const convertSlugToTitle = (slug: string): string => {
+	return slug
+		.split("-")
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(" ");
+};
