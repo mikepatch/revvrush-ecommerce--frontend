3bd9085b8131283f4e9e826dbc9c711691a2d02b
Michal Lata <michallata.dev@gmail.com>
2024-03-14 18:50:44 +0100

    patch: Update Header (to improve)

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -1,21 +1,58 @@
-import { MAIN_NAV_ITEMS } from "@/ui/constants";
+"use client";
+
+import { useState } from "react";
+import clsx from "clsx";
+import { LucideMenu, LucideSearch } from "lucide-react";
 import { BrandLogo } from "@/ui/atoms/BrandLogo";
 import { MainNavigation } from "@/ui/molecules/MainNavigation";
 import { CategoriesNavigation } from "@/ui/molecules/CategoriesNavigation";
-import { type NavItemType } from "@/ui/types";
+import { type NavItemType } from "@/types";
 
 type HeaderProps = {
 	categories: NavItemType[];
 };
 
 export const Header = ({ categories }: HeaderProps) => {
+	const [isCategoriesNavVisible, setIsCategoriesNavVisible] = useState(false);
+
+	const handleOpenCategoriesNav = () => {
+		setIsCategoriesNavVisible(!isCategoriesNavVisible);
+	};
+
 	return (
-		<header className="sticky left-0 top-0 z-50 flex flex-col items-center bg-brand-background-dark bg-gradient-to-b from-[#2d2d2d] shadow-sm">
-			<div className="flex w-full items-center justify-evenly py-2">
+		<header className="sticky left-0 right-0 top-0 z-50 flex h-header-height flex-col items-center justify-between bg-brand-background-dark bg-gradient-to-b from-[#2d2d2d] text-font-light shadow-sm">
+			<div className="flex w-full grow items-center justify-evenly">
 				<BrandLogo size="3xl" />
-				<MainNavigation navItems={MAIN_NAV_ITEMS} />
+				<form className="hidden items-center rounded-sm bg-brand-background-lighter sm:flex">
+					<LucideSearch className="p-1 text-font-dark" />
+					<input
+						type="search"
+						placeholder="Search"
+						className="h-full bg-brand-background-lighter p-2 text-font-dark"
+					/>
+				</form>
+				<MainNavigation />
 			</div>
-			<CategoriesNavigation categories={categories} />
+			<div className="flex w-full bg-brand-background-lighter sm:hidden">
+				<button onClick={handleOpenCategoriesNav} className="p-1 text-font-dark">
+					<LucideMenu className="p-1 text-font-dark" />
+				</button>
+				<form className="flex w-full items-center">
+					<LucideSearch className="p-1 text-font-dark" />
+					<input
+						type="search"
+						placeholder="Search"
+						className="h-full w-full bg-brand-background-lighter p-2 text-font-dark"
+					/>
+				</form>
+			</div>
+			<CategoriesNavigation
+				categories={categories}
+				className={clsx(
+					"absolute left-0 top-full z-50 transition-all sm:static sm:w-full",
+					isCategoriesNavVisible ? "translate-x-0" : "-translate-x-full sm:translate-x-0",
+				)}
+			/>
 		</header>
 	);
 };
