9188499904e8ddf906c1a4ff529185f4e866f2fe
Michal Lata <michallata.dev@gmail.com>
2024-03-18 12:54:03 +0100

    patch: Temporary replace CollectionsBanner with CollectionList

--- src/app/(with-navigation)/page.tsx
+++ src/app/(with-navigation)/page.tsx
@@ -1,8 +1,9 @@
 import { getAllProducts } from "@/api/products";
 import { PageTitle } from "@/ui/atoms/PageTitle";
 import { ProductList } from "@/ui/molecules/ProductList";
-import { CollectionsBanner } from "@/ui/organisms/CollectionsBannerCarousel";
+// import { CollectionsBanner } from "@/ui/organisms/CollectionsBannerCarousel";
 import { getAllCollections } from "@/api/collections";
+import { CollectionList } from "@/ui/organisms/CollectionList";
 
 export default async function HomePage() {
 	const { products } = await getAllProducts({ take: 4 });
@@ -10,7 +11,8 @@ export default async function HomePage() {
 
 	return (
 		<section className="flex flex-col gap-4">
-			<CollectionsBanner collections={collections} />
+			{/* <CollectionsBanner collections={collections} /> */}
+			<CollectionList collections={collections} />
 			<PageTitle title="RevvRush" />
 			<ProductList products={products.data} />
 		</section>
