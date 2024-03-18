083c9b68404f25b8801caee8a9b7e72c1d0593c4
Michal Lata <michallata.dev@gmail.com>
2024-03-18 13:41:52 +0100

    patch: Update PaginationItem

--- src/ui/molecules/PaginationItem.tsx
+++ src/ui/molecules/PaginationItem.tsx
@@ -23,7 +23,7 @@ export const PaginationItem = ({
 			className={`flex items-center border-t-2 border-slate-300 hover:border-brand-primary ${className}`}
 		>
 			<ActiveLink
-				href={`/categories/${categorySlug}/${pageNumber}` as Route}
+				href={`/${categorySlug}/${pageNumber}` as Route}
 				className="flex px-2 pt-2 leading-none transition-all hover:text-brand-primary sm:px-4 sm:pt-4"
 				activeClassName={clsx(
 					className === "" && "-mt-[2px] border-t-2 border-brand-primary text-brand-primary",
