3cbc9b1f075da1d4588e5d4de58d4848f50f9a39
Michal Lata <michallata.dev@gmail.com>
2024-03-18 13:45:22 +0100

    patch: Update CategoriesNavigation

--- src/ui/molecules/CategoriesNavigation.tsx
+++ src/ui/molecules/CategoriesNavigation.tsx
@@ -1,37 +1,42 @@
 import { ActiveLink } from "@/ui/atoms/ActiveLink";
 import { type NavItemType } from "@/types";
 
+type CategoriesNavigationProps = {
+	categories: NavItemType[];
+	className: React.ComponentProps<"nav">["className"];
+};
+
 export const CategoriesNavigation = ({
 	categories,
 	className,
-}: {
-	categories: NavItemType[];
-	className: React.ComponentProps<"nav">["className"];
-}) => {
-	categories.sort((a, b) => {
-		if (a.label === "All") return -1;
-		if (b.label === "All") return 1;
-		return 0;
-	});
-
+}: CategoriesNavigationProps) => {
 	return (
 		<nav className={`${className} sm:h-10`}>
 			<ul className="flex h-full w-full flex-col justify-center bg-font-light px-4 py-4 text-font-dark sm:flex-row sm:py-0">
 				<li>
 					<ActiveLink
 						href="/"
-						className="flex h-full items-center border-brand-primary py-1 transition-all hover:border-b-2 sm:px-2"
-						activeClassName="border-b-2"
+						className="flex h-full items-center border-b-2 py-1 transition-all hover:border-brand-primary sm:px-2"
+						activeClassName="border-brand-primary"
 					>
 						Home
+					</ActiveLink>
+				</li>
+				<li>
+					<ActiveLink
+						href="/products"
+						className="flex h-full items-center border-b-2 py-1 transition-all hover:border-brand-primary sm:px-2"
+						activeClassName="border-brand-primary"
+					>
+						All
 					</ActiveLink>
 				</li>
 				{categories.map(({ id, href, label }) => (
 					<li key={id} className="h-full">
 						<ActiveLink
 							href={href}
-							className="flex h-full items-center border-brand-primary py-1 transition-all hover:border-b-2 sm:px-2"
-							activeClassName="border-b-2"
+							className="flex h-full items-center border-b-2 py-1 transition-all hover:border-brand-primary sm:px-2"
+							activeClassName="border-brand-primary"
 							ariaLabel={label}
 						>
 							{label}
