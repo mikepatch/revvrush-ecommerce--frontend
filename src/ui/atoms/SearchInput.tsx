a8d3166d54de4041244d02981f552130ed6888c7
Michal Lata <michallata.dev@gmail.com>
2024-03-18 10:01:07 +0100

    patch: Add logic to SearchInput

--- src/ui/atoms/SearchInput.tsx
+++ src/ui/atoms/SearchInput.tsx
@@ -1,10 +1,44 @@
-import clsx from "clsx";
+"use client";
 
-export const SearchInput = ({ className, ...props }: { className?: string }) => {
+import clsx from "clsx";
+import { LucideSearch } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+type SearchInputProps = {
+	className?: string;
+};
+
+export const SearchInput = ({ className, ...props }: SearchInputProps) => {
+	const searchParams = useSearchParams();
+	const router = useRouter();
+
+	const handleSearch = (term: string) => {
+		const params = new URLSearchParams(searchParams);
+
+		if (term) {
+			params.set("query", term);
+		} else {
+			params.delete("query");
+		}
+
+		router.replace(`/search?${params.toString()}`);
+	};
+
 	return (
-		<input
-			{...props}
-			className={clsx("w-full border-2 border-brand-primary px-2 py-1", className)}
-		/>
+		<div className="flex h-full w-full items-center rounded-sm bg-brand-background-lighter text-font-dark">
+			<label htmlFor="search" className="p-2">
+				<LucideSearch />
+			</label>
+			<input
+				type="search"
+				id="search"
+				name="search"
+				placeholder="Search"
+				onChange={(e) => handleSearch(e.target.value)}
+				defaultValue={searchParams.get("query")?.toString()}
+				className={clsx("h-full w-full bg-transparent p-2", className)}
+				{...props}
+			/>
+		</div>
 	);
 };
