748610ae4281550237bbcdfbf6729dc261acccde
Michal Lata <michallata.dev@gmail.com>
2024-03-18 10:12:31 +0100

    patch: Update Header component

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import clsx from "clsx";
-import { LucideMenu, LucideSearch } from "lucide-react";
+import { LucideMenu } from "lucide-react";
 import { BrandLogo } from "@/ui/atoms/BrandLogo";
 import { MainNavigation } from "@/ui/molecules/MainNavigation";
 import { CategoriesNavigation } from "@/ui/molecules/CategoriesNavigation";
 import { type NavItemType } from "@/types";
+import { SearchInput } from "@/ui/atoms/SearchInput";
 
 type HeaderProps = {
 	categories: NavItemType[];
@@ -23,28 +24,22 @@ export const Header = ({ categories }: HeaderProps) => {
 		<header className="sticky left-0 right-0 top-0 z-50 flex h-header-height flex-col items-center justify-between bg-brand-background-dark bg-gradient-to-b from-[#2d2d2d] text-font-light shadow-sm">
 			<div className="flex w-full grow items-center justify-evenly">
 				<BrandLogo size="3xl" />
-				<form className="hidden items-center rounded-sm bg-brand-background-lighter sm:flex">
-					<LucideSearch className="p-1 text-font-dark" />
-					<input
-						type="search"
-						placeholder="Search"
-						className="h-full bg-brand-background-lighter p-2 text-font-dark"
-					/>
-				</form>
+				<div className="hidden items-center sm:flex">
+					<Suspense>
+						<SearchInput />
+					</Suspense>
+				</div>
 				<MainNavigation />
 			</div>
 			<div className="flex w-full bg-brand-background-lighter sm:hidden">
 				<button onClick={handleOpenCategoriesNav} className="p-1 text-font-dark">
 					<LucideMenu className="p-1 text-font-dark" />
 				</button>
-				<form className="flex w-full items-center">
-					<LucideSearch className="p-1 text-font-dark" />
-					<input
-						type="search"
-						placeholder="Search"
-						className="h-full w-full bg-brand-background-lighter p-2 text-font-dark"
-					/>
-				</form>
+				<div className="h-full w-full">
+					<Suspense>
+						<SearchInput />
+					</Suspense>
+				</div>
 			</div>
 			<CategoriesNavigation
 				categories={categories}
