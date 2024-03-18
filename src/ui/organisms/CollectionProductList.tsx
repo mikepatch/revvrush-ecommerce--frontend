ef038d3d464d799b9fc262827294c976004befcc
Michal Lata <michallata.dev@gmail.com>
2024-03-18 13:41:30 +0100

    patch: Update CollectionProductList

--- src/ui/organisms/CollectionProductList.tsx
+++ src/ui/organisms/CollectionProductList.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import { ProductCard } from "@/ui/molecules/ProductCard";
 import { Heading } from "@/ui/atoms/Heading";
 import { type ProductListItemFragment } from "@/gql/graphql";
+import { ProductList } from "@/ui/molecules/ProductList";
 
 type CollectionProductListProps = {
 	products: ProductListItemFragment[];
@@ -13,15 +12,7 @@ export const CollectionProductList = ({ products, title }: CollectionProductList
 		<section>
 			{title && <Heading level={2}>{title}</Heading>}
 			<section className="p-4">
-				<ul className="grid grid-cols-1 justify-center justify-items-center gap-4 sm:grid-cols-4">
-					{products.map((product) => (
-						<li key={product.id} className="group w-full max-w-[400px]">
-							<Link href={`/product/${product.id}`}>
-								<ProductCard product={product} />
-							</Link>
-						</li>
-					))}
-				</ul>
+				<ProductList products={products} />
 			</section>
 		</section>
 	);
