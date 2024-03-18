4c5877349557658be3a7ff8917f85676567df991
Michal Lata <michallata.dev@gmail.com>
2024-03-18 13:42:17 +0100

    patch: Temporary disable aside in CategoriesLayout

--- src/app/(with-navigation)/categories/layout.tsx
+++ src/app/(with-navigation)/categories/layout.tsx
@@ -14,12 +14,12 @@ type ProductsLayoutProps = {
 
 export default async function CategoriesLayout({ children }: ProductsLayoutProps) {
 	return (
-		<div className="mx-auto w-full max-w-7xl grid-cols-12 gap-x-8 text-font-dark lg:grid">
-			<aside className="col-span-3 h-fit rounded-sm bg-brand-background-lighter px-8 py-6 shadow-sm">
+		<div className="mx-auto text-font-dark">
+			{/* <aside className="col-span-3 h-fit rounded-sm bg-brand-background-lighter px-8 py-6 shadow-sm">
 				<h2 className="mb-4 text-xl font-semibold">Recommended products</h2>
 				<ul></ul>
-			</aside>
-			<section className="col-span-9 flex flex-grow flex-col gap-4 px-8 py-4">{children}</section>
+			</aside> */}
+			<section className="flex flex-grow flex-col gap-4 px-8 py-4">{children}</section>
 		</div>
 	);
 }
