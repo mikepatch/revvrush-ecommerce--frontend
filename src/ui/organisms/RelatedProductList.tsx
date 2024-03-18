39bf49ddc5078ad0462009ab2879639f2e827d0b
Michal Lata <michallata.dev@gmail.com>
2024-03-18 13:41:13 +0100

    feat: Add RelatedProductList

---
+++ src/ui/organisms/RelatedProductList.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+import { type ProductListItemFragment } from "@/gql/graphql";
+import { Heading } from "@/ui/atoms/Heading";
+import { ProductCard } from "@/ui/molecules/ProductCard";
+
+type RelatedProductListProps = {
+	products: ProductListItemFragment[];
+	title?: string;
+};
+
+export const RelatedProductList = ({ products, title }: RelatedProductListProps) => {
+	return (
+		<section data-testid="related-products">
+			{title && <Heading level={2}>{title}</Heading>}
+			<section className="p-4">
+				<ul className="flex gap-4">
+					{products.map((product) => (
+						<li key={product.id} className="group w-full max-w-[350px]">
+							<Link href={`/product/${product.id}`}>
+								<ProductCard product={product} />
+							</Link>
+						</li>
+					))}
+				</ul>
+			</section>
+		</section>
+	);
+};
