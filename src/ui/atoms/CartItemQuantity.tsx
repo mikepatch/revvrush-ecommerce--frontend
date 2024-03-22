878f4217a37c82fedfb7c7527b2cbc437ee8a5a5
Michal Lata <michallata.dev@gmail.com>
2024-03-22 19:39:06 +0100

    feat: Add CartItemQuantity

---
+++ src/ui/atoms/CartItemQuantity.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useOptimistic } from "react";
+import { changeItemQuantity } from "@/app/(with-navigation)/cart/actions";
+
+type CartItemQuantityProps = {
+	itemId: string;
+	quantity: number;
+};
+
+export const CartItemQuantity = ({ itemId, quantity }: CartItemQuantityProps) => {
+	const [optimisticQuantity, setOptimisticQuantity] = useOptimistic(
+		quantity,
+		(_state, newQuantity: number) => newQuantity,
+	);
+
+	return (
+		<form>
+			<button
+				type="submit"
+				formAction={async () => {
+					const newValue = optimisticQuantity - 1;
+
+					setOptimisticQuantity(newValue);
+					await changeItemQuantity(itemId, newValue);
+				}}
+				className="rounded-sm bg-gray-200 p-2 text-gray-800"
+			>
+				-
+			</button>
+			<span className="w-8 text-center">{optimisticQuantity}</span>
+			<button
+				type="submit"
+				formAction={async () => {
+					const newValue = optimisticQuantity + 1;
+
+					setOptimisticQuantity(newValue);
+					await changeItemQuantity(itemId, newValue);
+				}}
+				className="rounded-sm bg-gray-200 p-2 text-gray-800"
+			>
+				+
+			</button>
+		</form>
+	);
+};
