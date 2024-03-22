104ea9d127a9f2bcd69f5ac0cde7e5197ea8536b
Michal Lata <michallata.dev@gmail.com>
2024-03-22 19:39:18 +0100

    patch: Update MainNavigation

--- src/ui/molecules/MainNavigation.tsx
+++ src/ui/molecules/MainNavigation.tsx
@@ -1,23 +1,35 @@
-import { MAIN_NAV_ITEMS } from "@/constants";
+import { ShoppingBasket, User } from "lucide-react";
 import { ActiveLink } from "@/ui/atoms/ActiveLink";
 
-export const MainNavigation = () => {
+export const MainNavigation = async () => {
+	await Promise.resolve();
 	return (
 		<div className="h-full">
 			<ul className="flex h-full items-center">
-				{MAIN_NAV_ITEMS.map(({ id, href, label, icon }) => (
-					<li key={id} className="h-full">
-						<ActiveLink
-							href={href}
-							className="flex h-full flex-col items-center justify-center border-brand-primary px-2 py-1 transition-all hover:border-b-4"
-							activeClassName="border-b-4"
-							ariaLabel={label}
-						>
-							<div>{icon && icon}</div>
-							<span className="text-xs">{label}</span>
-						</ActiveLink>
-					</li>
-				))}
+				<li className="h-full">
+					<ActiveLink
+						href="/account"
+						className="flex h-full flex-col items-center justify-center gap-1 border-b-4 border-transparent px-2 py-1 transition-all hover:border-brand-primary"
+						activeClassName="!border-brand-primary"
+						ariaLabel="Your account"
+					>
+						<div>
+							<User />
+						</div>
+						<span className="text-xs">Your account</span>
+					</ActiveLink>
+				</li>
+				<li className="h-full">
+					<ActiveLink
+						href="/cart"
+						className="flex h-full flex-col items-center justify-center gap-1 border-b-4 border-transparent px-2 py-1 transition-all hover:border-brand-primary"
+						activeClassName="!border-brand-primary"
+						ariaLabel="Cart"
+					>
+						<ShoppingBasket />
+						<span className="text-xs">Cart (0)</span>
+					</ActiveLink>
+				</li>
 			</ul>
 		</div>
 	);
