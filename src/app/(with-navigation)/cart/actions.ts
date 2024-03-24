e981b2f33c33f5da1fe72325df1185f7fbfdfb72
Michal Lata <michallata.dev@gmail.com>
2024-03-24 19:58:15 +0100

    patch: Update cart actions

--- src/app/(with-navigation)/cart/actions.ts
+++ src/app/(with-navigation)/cart/actions.ts
@@ -1,8 +1,29 @@
 "use server";
 
+import { revalidateTag } from "next/cache";
 import { executeGraphQL } from "@/api/graphqlApi";
-import { CartUpdateItemDocument } from "@/gql/graphql";
+import { CartItemRemoveFromCartDocument, CartUpdateItemDocument } from "@/gql/graphql";
 
 export const changeItemQuantity = (cartItemId: string, quantity: number) => {
-	return executeGraphQL(CartUpdateItemDocument, { cartItemId, data: { quantity } });
+	revalidateTag("cart");
+
+	return executeGraphQL({
+		query: CartUpdateItemDocument,
+		variables: { cartItemId, data: { quantity } },
+		next: {
+			tags: ["cart"],
+		},
+	});
+};
+
+export const removeItemFromCart = (cartItemId: string) => {
+	revalidateTag("cart");
+
+	return executeGraphQL({
+		query: CartItemRemoveFromCartDocument,
+		variables: { cartItemId },
+		next: {
+			tags: ["cart"],
+		},
+	});
 };
