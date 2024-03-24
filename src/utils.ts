8e86d3899521bbe7184f2fac5c7f59f55c8aa0cd
Michal Lata <michallata.dev@gmail.com>
2024-03-24 19:53:42 +0100

    feat: Add sumItemsQuantityInCart util

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { type CartWithMetadataFragment } from "@/gql/graphql";
+
 export const formatPrice = (amount: number): string => {
 	return new Intl.NumberFormat("en-US", {
 		style: "currency",
@@ -11,4 +13,17 @@ export const convertSlugToTitle = (slug: string): string => {
 		.split("-")
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
 		.join(" ");
+};
+
+export const sumItemsQuantityInCart = (
+	items: CartWithMetadataFragment["data"]["items"],
+): number => {
+	return (
+		items.reduce((acc, item) => {
+			if (item) {
+				return acc + item.quantity;
+			}
+			return acc;
+		}, 0) ?? 0
+	);
 };
