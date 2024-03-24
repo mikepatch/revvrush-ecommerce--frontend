c6b69cd9263567ac94f7e9113ba6a392ba06b3c0
Michal Lata <michallata.dev@gmail.com>
2024-03-24 19:55:23 +0100

    patch: Update CartPage

--- src/app/(with-navigation)/cart/page.tsx
+++ src/app/(with-navigation)/cart/page.tsx
@@ -1,9 +1,11 @@
 import { redirect } from "next/navigation";
 import NextImage from "next/image";
 
+import Link from "next/link";
 import { getCartFromCookies } from "@/api/cart";
 import { formatPrice } from "@/utils";
 import { CartItemQuantity } from "@/ui/atoms/CartItemQuantity";
+import { RemoveFromCartButton } from "@/ui/atoms/RemoveFromCartButton";
 
 export default async function CartPage() {
 	const cart = await getCartFromCookies();
@@ -12,10 +14,10 @@ export default async function CartPage() {
 	}
 
 	return (
-		<>
-			<table>
+		<section className="flex justify-center">
+			<table className="table-auto border-spacing-x-4 border-spacing-y-4">
 				<thead>
-					<tr>
+					<tr className="table-row ">
 						<th>Product</th>
 						<th>Quantity</th>
 						<th>Price</th>
@@ -27,24 +29,32 @@ export default async function CartPage() {
 						cart.data.items.map(
 							(item) =>
 								item && (
-									<tr key={item.id}>
-										<td className="flex items-center gap-2">
-											<NextImage
-												alt={item.product.name}
-												src={(item.product.images as string[])[0] || ""}
-												width={50}
-												height={50}
-											/>
-											{item.product.name}
+									<tr key={item.id} className="border border-slate-200 p-4">
+										<td className="p-4">
+											<Link
+												href={`/product/${item.product.id}`}
+												className="flex items-center gap-2"
+											>
+												<NextImage
+													alt={item.product.name}
+													src={(item.product.images as string[])[0] || ""}
+													width={50}
+													height={50}
+												/>
+												{item.product.name}
+											</Link>
 										</td>
 										<td className="text-center">
-											<div className="flex items-center">
+											<div className="flex items-center justify-center">
 												<CartItemQuantity itemId={item.id} quantity={item.quantity} />
 											</div>
 										</td>
 										<td className="text-right">{formatPrice(item.product.price)}</td>
 										<td className="text-right">
 											{formatPrice(item.quantity * item.product.price)}
+										</td>
+										<td className="">
+											<RemoveFromCartButton productId={item.id} />
 										</td>
 									</tr>
 								),
@@ -55,12 +65,11 @@ export default async function CartPage() {
 				</tbody>
 				<tfoot>
 					<tr>
-						<td colSpan={3}>Total</td>
+						<th>Total</th>
 						<td>{formatPrice(cart.meta.totalPrice)}</td>
 					</tr>
 				</tfoot>
 			</table>
-			{/* <pre>{JSON.stringify(cart, null, 2)}</pre> */}
-		</>
+		</section>
 	);
 }
