53e6baa2c060bc81b5e6008f6490b11470df10da
Michal Lata <michallata.dev@gmail.com>
2024-03-24 19:56:48 +0100

    feat: Add HeaderTopNavbar

---
+++ src/ui/molecules/HeaderTopNavbar.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+
+import { BrandLogo } from "@/ui/atoms/BrandLogo";
+import { SearchInput } from "@/ui/atoms/SearchInput";
+import { MainNavigation } from "@/ui/molecules/MainNavigation";
+
+export const HeaderTopNavbar = () => {
+	return (
+		<div className="flex w-full grow items-center justify-evenly">
+			<BrandLogo size="3xl" />
+			<div className="hidden items-center sm:flex">
+				<Suspense>
+					<SearchInput variant="desktop" />
+				</Suspense>
+			</div>
+			<MainNavigation />
+		</div>
+	);
+};
