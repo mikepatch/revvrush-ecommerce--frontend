9376d4e7b7c9868a16a65d84bd4487cd540895c5
Michal Lata <michallata.dev@gmail.com>
2024-03-24 19:57:04 +0100

    patch: Update MainNavigation

--- src/ui/molecules/MainNavigation.tsx
+++ src/ui/molecules/MainNavigation.tsx
@@ -1,35 +1,29 @@
-import { ShoppingBasket, User } from "lucide-react";
-import { ActiveLink } from "@/ui/atoms/ActiveLink";
+import { ShoppingCart, User } from "lucide-react";
+
+import { getCartFromCookies } from "@/api/cart";
+import { MainNavItem } from "@/ui/atoms/MainNavItem";
+import { sumItemsQuantityInCart } from "@/utils";
 
 export const MainNavigation = async () => {
-	await Promise.resolve();
+	const cart = await getCartFromCookies();
+	const quantity = sumItemsQuantityInCart(cart?.data?.items ?? []);
+
 	return (
 		<div className="h-full">
 			<ul className="flex h-full items-center">
-				<li className="h-full">
-					<ActiveLink
-						href="/account"
-						className="flex h-full flex-col items-center justify-center gap-1 border-b-4 border-transparent px-2 py-1 transition-all hover:border-brand-primary"
-						activeClassName="!border-brand-primary"
-						ariaLabel="Your account"
-					>
-						<div>
-							<User />
-						</div>
-						<span className="text-xs">Your account</span>
-					</ActiveLink>
-				</li>
-				<li className="h-full">
-					<ActiveLink
-						href="/cart"
-						className="flex h-full flex-col items-center justify-center gap-1 border-b-4 border-transparent px-2 py-1 transition-all hover:border-brand-primary"
-						activeClassName="!border-brand-primary"
-						ariaLabel="Cart"
-					>
-						<ShoppingBasket />
-						<span className="text-xs">Cart (0)</span>
-					</ActiveLink>
-				</li>
+				<MainNavItem href="/account" ariaLabel="Your account">
+					<User />
+					<span className="text-xs">Account</span>
+				</MainNavItem>
+				<MainNavItem href="/cart" ariaLabel="Cart">
+					{quantity > 0 && (
+						<span className="absolute right-0 top-1 rounded-full bg-brand-primary px-1 text-xs text-font-light">
+							{quantity}
+						</span>
+					)}
+					<ShoppingCart />
+					<span className="text-xs">Cart</span>
+				</MainNavItem>
 			</ul>
 		</div>
 	);
