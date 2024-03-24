059a6be86d5cd2e57d1376dce4c00995ce038a18
Michal Lata <michallata.dev@gmail.com>
2024-03-24 19:56:39 +0100

    feat: Add HeaderBottomNavbar

---
+++ src/ui/molecules/HeaderBottomNavbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Suspense, useState } from "react";
+
+import { SearchInput } from "@/ui/atoms/SearchInput";
+import { CategoriesNavigation } from "@/ui/molecules/CategoriesNavigation";
+import { type NavItemType } from "@/types";
+import { CategoriesNavToggleButton } from "@/ui/atoms/CategoriesNavToggleButton";
+
+type HeaderBottomNavbarProps = {
+	categories: NavItemType[];
+};
+
+export const HeaderBottomNavbar = async ({ categories }: HeaderBottomNavbarProps) => {
+	const [isCategoriesNavVisible, setIsCategoriesNavVisible] = useState(false);
+
+	const handleOpenCategoriesNav = () => {
+		setIsCategoriesNavVisible(!isCategoriesNavVisible);
+	};
+
+	return (
+		<>
+			<div className="flex w-full bg-brand-background-lighter sm:hidden">
+				<CategoriesNavToggleButton onToggle={handleOpenCategoriesNav} />
+				<div className="h-full w-full">
+					<Suspense>
+						<SearchInput variant="mobile" />
+					</Suspense>
+				</div>
+			</div>
+			<CategoriesNavigation categories={categories} isVisible={isCategoriesNavVisible} />
+		</>
+	);
+};
