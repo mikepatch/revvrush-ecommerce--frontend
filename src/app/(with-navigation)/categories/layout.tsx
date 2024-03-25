c40749144f557227f730a1bf98cc267aa4fe1cf8
Michal Lata <michallata.dev@gmail.com>
2024-03-25 18:52:27 +0100

    patch: Update padding in CategoriesLayout

--- src/app/(with-navigation)/categories/layout.tsx
+++ src/app/(with-navigation)/categories/layout.tsx
@@ -12,14 +12,14 @@ type ProductsLayoutProps = {
 	children: React.ReactNode;
 };
 
-export default async function CategoriesLayout({ children }: ProductsLayoutProps) {
+export default function CategoriesLayout({ children }: ProductsLayoutProps) {
 	return (
 		<div className="mx-auto text-font-dark">
 			{/* <aside className="col-span-3 h-fit rounded-sm bg-brand-background-lighter px-8 py-6 shadow-sm">
 				<h2 className="mb-4 text-xl font-semibold">Recommended products</h2>
 				<ul></ul>
 			</aside> */}
-			<section className="flex flex-grow flex-col gap-4 px-8 py-4">{children}</section>
+			<section className="flex flex-grow flex-col gap-4 px-8">{children}</section>
 		</div>
 	);
 }
