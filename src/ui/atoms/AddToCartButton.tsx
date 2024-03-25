e537a44de1379c7fd3fbb07c5264d96d3d3a3862
Michal Lata <michallata.dev@gmail.com>
2024-03-25 18:55:16 +0100

    patch: Update AddToCartButton styles

--- src/ui/atoms/AddToCartButton.tsx
+++ src/ui/atoms/AddToCartButton.tsx
@@ -11,7 +11,7 @@ export const AddToCartButton = () => {
 			aria-label="Add to cart"
 			type="submit"
 			disabled={status.pending}
-			className="group rounded-sm bg-brand-primary px-4 py-3 text-font-light shadow-sm transition-all hover:brightness-105 disabled:cursor-wait disabled:opacity-50"
+			className="group rounded-md bg-brand-primary px-4 py-3 text-font-light shadow-sm transition-all hover:brightness-105 disabled:cursor-wait disabled:opacity-50"
 		>
 			<div className="flex items-center gap-2">
 				<ShoppingCart size={24} />
