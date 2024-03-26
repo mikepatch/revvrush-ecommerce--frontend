dffe914478106e0f3153682e0aa44945bdf1216a
Michal Lata <michallata.dev@gmail.com>
2024-03-26 20:08:09 +0100

    patch: Update CollectionList

--- src/ui/organisms/CollectionList.tsx
+++ src/ui/organisms/CollectionList.tsx
@@ -8,24 +8,22 @@ type CollectionListProps = {
 };
 
 export const CollectionList = ({ collections }: CollectionListProps) => (
-	<div>
-		<ul className="flex justify-center gap-4">
-			{collections.map((collection) => (
-				<li key={collection.id} className="rounded-sm bg-brand-background-dark">
-					<Link href={`/collections/${collection.slug}`}>
-						<article>
-							<h2 className="px-2 py-1 text-font-light">{collection.name}</h2>
-							<NextImage
-								src={collection.coverImage}
-								width={300}
-								height={200}
-								alt=""
-								className="rounded-sm"
-							/>
-						</article>
-					</Link>
-				</li>
-			))}
-		</ul>
-	</div>
+	<ul className="flex justify-center gap-4">
+		{collections.map((collection) => (
+			<li key={collection.id} className="rounded-sm bg-brand-background-dark">
+				<Link href={`/collections/${collection.slug}`}>
+					<article>
+						<h2 className="px-2 py-1 text-font-light">{collection.name}</h2>
+						<NextImage
+							src={collection.coverImage}
+							width={300}
+							height={200}
+							alt=""
+							className="rounded-sm"
+						/>
+					</article>
+				</Link>
+			</li>
+		))}
+	</ul>
 );
