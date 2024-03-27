cbfc3e8d213eb52bbef17e8940b6624b80e69e0c
Michal Lata <michallata.dev@gmail.com>
2024-03-27 18:13:09 +0100

    patch: Tweak CategoriesNavigation styles

--- src/ui/molecules/CategoriesNavigation.tsx
+++ src/ui/molecules/CategoriesNavigation.tsx
@@ -16,11 +16,11 @@ export const CategoriesNavigation = ({ categories, isVisible }: CategoriesNaviga
 				isVisible ? "translate-x-0" : "-translate-x-full sm:translate-x-0",
 			)}
 		>
-			<ul className="flex h-full w-full flex-col justify-center bg-brand-background-lighter px-4 py-4 text-font-dark sm:flex-row sm:py-0">
+			<ul className="flex h-full w-full flex-col justify-center gap-4 bg-brand-background-lighter px-4 py-4 text-font-dark sm:flex-row sm:py-0">
 				<li>
 					<ActiveLink
 						href="/"
-						className="flex h-full items-center border-b-2 border-transparent py-1 transition-all hover:border-brand-primary sm:px-2"
+						className="flex h-full items-center border-b-2 border-transparent py-1 transition-colors hover:border-brand-primary sm:px-2"
 						activeClassName="!border-brand-primary"
 					>
 						Home
@@ -29,7 +29,7 @@ export const CategoriesNavigation = ({ categories, isVisible }: CategoriesNaviga
 				<li>
 					<ActiveLink
 						href="/products"
-						className="flex h-full items-center border-b-2 border-transparent py-1 transition-all hover:border-brand-primary sm:px-2"
+						className="flex h-full items-center border-b-2 border-transparent py-1 transition-colors hover:border-brand-primary sm:px-2"
 						activeClassName="!border-brand-primary"
 					>
 						All
@@ -39,7 +39,7 @@ export const CategoriesNavigation = ({ categories, isVisible }: CategoriesNaviga
 					<li key={id} className="h-full">
 						<ActiveLink
 							href={href}
-							className="flex h-full items-center border-b-2 border-transparent py-1 transition-all hover:border-brand-primary sm:px-2"
+							className="flex h-full items-center border-b-2 border-transparent py-1 transition-colors hover:border-brand-primary sm:px-2"
 							activeClassName="!border-brand-primary"
 							ariaLabel={label}
 						>
