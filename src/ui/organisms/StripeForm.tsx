6693427c2f9d6affdba2260859b67b83be766a3a
Michal Lata <michallata.dev@gmail.com>
2024-03-27 18:09:38 +0100

    feat: Add StripeForm

---
+++ src/ui/organisms/StripeForm.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+import { CheckoutForm } from "@/ui/molecules/CheckoutForm";
+
+if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+	throw new Error("Missing STRIPE_PUBLISHABLE_KEY env variable");
+}
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+type StripeFormProps = {
+	clientSecret: string;
+	totalAmount: string;
+};
+
+export const StripeForm = ({ clientSecret, totalAmount }: StripeFormProps) => {
+	return (
+		<Elements options={{ appearance: { theme: "stripe" }, clientSecret }} stripe={stripePromise}>
+			<CheckoutForm totalAmount={totalAmount} />
+		</Elements>
+	);
+};
