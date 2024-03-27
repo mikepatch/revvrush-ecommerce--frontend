bc8c3402f5a6cec434d55a349712d52413746a37
Michal Lata <michallata.dev@gmail.com>
2024-03-27 18:11:26 +0100

    patch: Update z-index in Header

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -8,7 +8,7 @@ type HeaderProps = {
 
 export const Header = ({ categories }: HeaderProps) => {
 	return (
-		<header className="sticky left-0 right-0 top-0 z-50 flex h-header-height flex-col items-center justify-between bg-brand-background-dark bg-gradient-to-b from-[#2d2d2d] text-font-light shadow-sm">
+		<header className="sticky left-0 right-0 top-0 z-20 flex h-header-height flex-col items-center justify-between bg-brand-background-dark bg-gradient-to-b from-[#2d2d2d] text-font-light shadow-sm">
 			<HeaderTopNavbar />
 			<HeaderBottomNavbar categories={categories} />
 		</header>
