de55cc8f13ca27621bf833df4d823780f1349c86
Michal Lata <michallata.dev@gmail.com>
2024-03-27 18:12:20 +0100

    patch: Update ProductDetailsHeader (WIP)

--- src/ui/molecules/ProductDetailsHeader.tsx
+++ src/ui/molecules/ProductDetailsHeader.tsx
@@ -1,35 +1,39 @@
 import { revalidateTag } from "next/cache";
 
-import { ProductImage } from "@/ui/atoms/ProductImage";
 import { formatPrice } from "@/utils";
 import { type ProductListItemFragment } from "@/gql/graphql";
 import { addProductToCart, getOrCreateCart } from "@/api/cart";
 import { AddToCartButton } from "@/ui/atoms/AddToCartButton";
+import { ProductDetailsImage } from "@/ui/atoms/ProductDetailsImage";
 
 type ProductsDetailsHeaderProps = {
 	product: ProductListItemFragment;
 };
 
 export const ProductDetailsHeader = ({ product }: ProductsDetailsHeaderProps) => {
-	const addProductToCartAction = async () => {
+	const addProductToCartAction = async (formData: FormData) => {
 		"use server";
 
-		const cart = await getOrCreateCart();
+		try {
+			const cart = await getOrCreateCart();
+			const quantity = Number(formData.get("quantity"));
 
-		await addProductToCart(cart.id, product.id, 1);
-		console.log(product.id);
-		revalidateTag("cart");
+			await addProductToCart(cart.data.id, product.id, quantity);
+			revalidateTag("cart");
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	return (
 		<header className="flex flex-col justify-between gap-6 sm:flex-row">
 			<div className="min-w-1/2 group bg-white">
 				{product.images && product.images[0] && (
-					<ProductImage
+					<ProductDetailsImage
 						src={product.images[0]}
 						alt={product.name}
-						width={640}
-						height={640}
+						width={300}
+						height={300}
 						resizeOnHover
 					/>
 				)}
@@ -53,6 +57,21 @@ export const ProductDetailsHeader = ({ product }: ProductsDetailsHeaderProps) =>
 				<div className="flex flex-wrap items-center justify-between rounded-sm p-4">
 					<p className=" text-2xl font-semibold">{formatPrice(product.price)}</p>
 					<form action={addProductToCartAction}>
+						{/* <div className="flex items-center">
+							<button type="button" className="rounded-l bg-gray-200 px-2 py-1">
+								-
+							</button>
+							<input
+								type="number"
+								name="quantity"
+								defaultValue="1"
+								className="w-16 bg-gray-200 px-2 py-1 text-center"
+							/>
+							<button type="button" className="rounded-r bg-gray-200 px-2 py-1">
+								+
+							</button>
+						</div> */}
+						<input type="number" name="quantity" defaultValue="1" className="w-12 px-2 py-2" />
 						<AddToCartButton />
 					</form>
 				</div>
