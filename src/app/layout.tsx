45e79339e6fbd05f24e9f4ab7c8e799b37410f47
Michal Lata <michallata.dev@gmail.com>
2024-03-29 18:46:33 +0100

    feat: Add metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import { Rubik } from "next/font/google";
 import "../styles/globals.css";
 import { Footer } from "@/ui/organisms/Footer";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+if (!baseUrl) {
+	throw new Error("NEXT_PUBLIC_BASE_URL is not set");
+}
+
 const rubik = Rubik({ subsets: ["latin"] });
 
 type RootLayoutProps = {
@@ -12,10 +18,10 @@ type RootLayoutProps = {
 };
 
 export const metadata: Metadata = {
+	metadataBase: new URL(baseUrl),
 	title: "RevvRush – Tune Up Your Car!",
 	description: "RevvRush - the best car parts in the world!",
 };
-
 export default function RootLayout({ children, modal }: RootLayoutProps) {
 	return (
 		<html lang="en">
