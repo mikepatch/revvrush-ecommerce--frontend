ae46f6d1440430c49a8ff2b6cd7dc89bfbc0611e
Michal Lata <michallata.dev@gmail.com>
2024-04-02 18:44:02 +0200

    patch: Update ProductDeatilsHeader (WIP). Update the source of data, update styles, add StarRating

--- src/ui/molecules/ProductDetails/ProductDetailsHeader.tsx
+++ src/ui/molecules/ProductDetails/ProductDetailsHeader.tsx
@@ -1,24 +1,29 @@
-// import { revalidateTag } from "next/cache";
+import { type ProductWithDescriptionFragment } from "@/gql/graphql";
 
 import { formatPrice } from "@/utils";
-import { type ProductListItemFragment } from "@/gql/graphql";
-// import { addProductToCart, getOrCreateCart } from "@/api/cart";
 import { AddToCartButton } from "@/ui/atoms/AddToCartButton";
 import { ProductDetailsImage } from "@/ui/atoms/ProductDetailsImage";
-import { addProductToCartAction } from "@/actions";
+import { addProductToCartAction } from "@/app/actions";
+import { Heading } from "@/ui/atoms/Heading";
+import { StarRating } from "@/ui/molecules/StarRating";
 
 type ProductsDetailsHeaderProps = {
-	product: ProductListItemFragment;
+	product: ProductWithDescriptionFragment;
 };
 
 export const ProductDetailsHeader = ({ product }: ProductsDetailsHeaderProps) => {
+	const {
+		data: productData,
+		meta: { totalReviews },
+	} = product;
+
 	return (
 		<header className="flex flex-col justify-between gap-6 sm:flex-row">
 			<div className="min-w-1/2 group bg-white">
-				{product.images && product.images[0] && (
+				{productData.images && productData.images[0] && (
 					<ProductDetailsImage
-						src={product.images[0]}
-						alt={product.name}
+						src={productData.images[0]}
+						alt={productData.name}
 						width={300}
 						height={300}
 						resizeOnHover
@@ -26,11 +31,20 @@ export const ProductDetailsHeader = ({ product }: ProductsDetailsHeaderProps) =>
 				)}
 			</div>
 			<section className="flex grow flex-col justify-between">
-				<div className="flex w-full flex-col">
-					<h1 className="mb-4 text-3xl font-bold">{product.name}</h1>
-					<small className="text-xs text-gray-500" aria-disabled>
-						ID: {product.id}
-					</small>
+				<div className="flex w-full flex-col gap-2 border-b">
+					<Heading level={1}>{productData.name}</Heading>
+					<div>
+						<div className="flex gap-2">
+							<StarRating
+								rating={Math.floor(productData.avgRating || 0)}
+								variant="small"
+								label={`(${totalReviews} reviews)`}
+							/>{" "}
+						</div>
+						<small className="text-gray-500" aria-disabled>
+							ID: {productData.id}
+						</small>
+					</div>
 				</div>
 				{/* {product.stock > 0 ? (
 					<div className="flex gap-2">
@@ -44,7 +58,7 @@ export const ProductDetailsHeader = ({ product }: ProductsDetailsHeaderProps) =>
 					</div>
 				)} */}
 				<div className="flex flex-wrap items-center justify-between rounded-sm p-4">
-					<p className=" text-2xl font-semibold">{formatPrice(product.price)}</p>
+					<p className=" text-2xl font-semibold">{formatPrice(productData.price)}</p>
 					<form action={addProductToCartAction} className="flex">
 						{/* <div className="flex items-center">
 							<button type="button" className="rounded-l bg-gray-200 px-2 py-1">
@@ -67,7 +81,7 @@ export const ProductDetailsHeader = ({ product }: ProductsDetailsHeaderProps) =>
 							className="w-12 px-2 py-2"
 							aria-label="Set quantity"
 						/>
-						<input type="hidden" name="productId" value={product.id} aria-hidden />
+						<input type="hidden" name="productId" value={productData.id} aria-hidden />
 						<AddToCartButton variant="both" data-testid="add-to-cart-button" />
 					</form>
 				</div>
