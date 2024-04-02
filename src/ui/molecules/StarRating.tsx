302f83df7bc30b29beaf50c2b0a78fa392a76571
Michal Lata <michallata.dev@gmail.com>
2024-04-02 18:46:34 +0200

    patch: Update StarRating (WIP)

--- src/ui/molecules/StarRating.tsx
+++ src/ui/molecules/StarRating.tsx
@@ -3,16 +3,36 @@ import clsx from "clsx";
 type StarRatingProps = {
 	rating: number;
 	variant?: "big" | "small";
+	label?: string;
+	layout?: "row" | "col";
 };
 
-export const StarRating = ({ rating, variant = "small" }: StarRatingProps) => {
+export const StarRating = ({
+	rating,
+	variant = "small",
+	label,
+	layout = "row",
+}: StarRatingProps) => {
 	return (
-		<ul className={clsx("flex", variant === "big" ? "gap-2" : "gap-1")}>
-			{Array.from({ length: 5 }).map((_, index) => (
-				<li key={index + rating} className={clsx('text-star-rating', variant === "big" ? "text-2xl" : "text-sm")}>
-					{index >= rating ? <span className="">&#9734;</span> : <span className="">&#9733;</span>}
-				</li>
-			))}
-		</ul>
+		<div className={`flex flex-${layout} w-fit items-center gap-2`}>
+			<ul
+				className={clsx("flex w-fit", variant === "big" ? "gap-2" : "gap-1")}
+				aria-label={`Rating ${rating}`}
+			>
+				{Array.from({ length: 5 }).map((_, index) => (
+					<li
+						key={index + rating}
+						className={clsx("text-star-rating", variant === "big" ? "text-2xl" : "text-sm")}
+					>
+						{index >= rating ? (
+							<span className="">&#9734;</span>
+						) : (
+							<span className="">&#9733;</span>
+						)}
+					</li>
+				))}
+			</ul>
+			{label && <small data-testid="product-rating">{label}</small>}
+		</div>
 	);
 };
