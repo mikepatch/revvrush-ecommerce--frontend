13561d7104468ed9d8447dcd911b226ebf065526
Michal Lata <michallata.dev@gmail.com>
2024-04-02 18:37:29 +0200

    patch: Move action to app

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,7 +2,11 @@
 
 import { revalidateTag } from "next/cache";
 import { executeGraphQL } from "@/api/graphqlApi";
-import { CartItemRemoveFromCartDocument, CartUpdateItemDocument } from "@/gql/graphql";
+import {
+	CartItemRemoveFromCartDocument,
+	CartUpdateItemDocument,
+	ProductReviewCreateDocument,
+} from "@/gql/graphql";
 import { addProductToCart, getOrCreateCart } from "@/api/cart";
 
 export const changeItemQuantity = async (cartItemId: string, quantity: number) => {
@@ -36,4 +40,31 @@ export const addProductToCartAction = async (formData: FormData) => {
 
 	await addProductToCart(cart.data.id, productId, quantity);
 	revalidateTag("cart");
+};
+
+export const addProductReviewAction = async (formData: FormData) => {
+	"use server";
+
+	const productId = formData.get("productId") as string;
+	const rating = Number(formData.get("rating"));
+	const headline = formData.get("headline") as string;
+	const content = formData.get("content") as string;
+	const name = formData.get("name") as string;
+	const email = formData.get("email") as string;
+
+	await executeGraphQL({
+		query: ProductReviewCreateDocument,
+		variables: {
+			productId,
+			rating,
+			headline,
+			content,
+			name,
+			email,
+		},
+		next: {
+			tags: ["product"],
+		},
+	});
+	revalidateTag("product");
 };
