53ba209a059526834a478936df4df0dcb1bcbf1a
Michal Lata <michallata.dev@gmail.com>
2024-04-02 18:48:17 +0200

    feat: Implement sorting in product listing routes

--- src/app/(with-navigation)/categories/[categorySlug]/[pageNumber]/page.tsx
+++ src/app/(with-navigation)/categories/[categorySlug]/[pageNumber]/page.tsx
@@ -1,15 +1,22 @@
 import { notFound } from "next/navigation";
+import { type ProductOrderByWithRelationInput } from "@/gql/graphql";
+
 import { ProductList } from "@/ui/molecules/List/ProductList";
 import { Pagination } from "@/ui/molecules/Pagination/Pagination";
 import { PageTitle } from "@/ui/atoms/PageTitle";
-import { getAllProducts, getProductsByCategorySlug } from "@/api/products";
+import { getAllProducts } from "@/api/products";
 import { convertSlugToTitle } from "@/utils";
 import { PRODUCTS_ON_PAGE } from "@/constants";
+import { SortButton } from "@/ui/atoms/SortButton";
 
 type PageNumberPageProps = {
 	params: {
 		pageNumber: string;
 		categorySlug: string;
+	};
+	searchParams: {
+		sort_by?: "price" | "rating";
+		sort_order?: "asc" | "desc";
 	};
 };
 
@@ -31,17 +38,36 @@ export const generateMetadata = async ({ params }: PageNumberPageProps) => {
 	};
 };
 
-export default async function CategoryPageNumberPage({ params }: PageNumberPageProps) {
+export default async function CategoryPageNumberPage({
+	params,
+	searchParams,
+}: PageNumberPageProps) {
 	const { categorySlug } = params;
 	const pageNumber = Number(params.pageNumber);
+	const orderBy: ProductOrderByWithRelationInput = {
+		price: searchParams.sort_by === "price" ? searchParams.sort_order : undefined,
+		avgRating:
+			searchParams.sort_by === "rating"
+				? { sort: searchParams.sort_order || "asc", nulls: "last" }
+				: undefined,
+	};
 
 	const {
 		data: products,
 		meta: { total: totalProducts },
-	} = await getProductsByCategorySlug({
-		slug: categorySlug,
+	} = await getAllProducts({
 		take: PRODUCTS_ON_PAGE,
 		skip: (pageNumber - 1) * PRODUCTS_ON_PAGE,
+		where: {
+			category: {
+				is: {
+					slug: {
+						equals: categorySlug,
+					},
+				},
+			},
+		},
+		orderBy,
 	});
 
 	if (products.length === 0) {
@@ -53,7 +79,8 @@ export default async function CategoryPageNumberPage({ params }: PageNumberPageP
 			<header>
 				<PageTitle title={convertSlugToTitle(categorySlug)} />
 			</header>
-			<section>
+			<section className="flex flex-col items-end gap-2">
+				<SortButton />
 				<ProductList products={products} />
 			</section>
 			<footer>
--- src/app/(with-navigation)/products/[pageNumber]/page.tsx
+++ src/app/(with-navigation)/products/[pageNumber]/page.tsx
@@ -1,28 +1,52 @@
+import { type ProductOrderByWithRelationInput } from "@/gql/graphql";
+
 import { getAllProducts } from "@/api/products";
 import { PRODUCTS_ON_PAGE } from "@/constants";
-import { PageTitle } from "@/ui/atoms/PageTitle";
 import { Pagination } from "@/ui/molecules/Pagination/Pagination";
 import { ProductList } from "@/ui/molecules/List/ProductList";
+import { SortButton } from "@/ui/atoms/SortButton";
+import { Heading } from "@/ui/atoms/Heading";
 
 type ProductsPageNumberProps = {
 	params: {
 		pageNumber: string;
 	};
+	searchParams: {
+		sort_by?: "price" | "rating";
+		sort_order?: "asc" | "desc";
+	};
 };
 
-export default async function ProductsPageNumber({ params }: ProductsPageNumberProps) {
+export default async function ProductsPageNumber({
+	params,
+	searchParams,
+}: ProductsPageNumberProps) {
 	const pageNumber = Number(params.pageNumber);
+	const orderBy: ProductOrderByWithRelationInput = {
+		price: searchParams.sort_by === "price" ? searchParams.sort_order : undefined,
+		avgRating:
+			searchParams.sort_by === "rating"
+				? { sort: searchParams.sort_order || "asc", nulls: "last" }
+				: undefined,
+	};
 	const {
 		data: products,
 		meta: { total: totalProducts },
-	} = await getAllProducts({ take: PRODUCTS_ON_PAGE, skip: (pageNumber - 1) * PRODUCTS_ON_PAGE });
+	} = await getAllProducts({
+		take: PRODUCTS_ON_PAGE,
+		skip: (pageNumber - 1) * PRODUCTS_ON_PAGE,
+		orderBy,
+	});
 
 	return (
 		<>
 			<header>
-				<PageTitle title="Products" />
+				<Heading level={1}>
+					Products <small>({totalProducts})</small>
+				</Heading>
 			</header>
-			<section>
+			<section className="flex flex-col items-end gap-2">
+				<SortButton />
 				<ProductList products={products} />
 			</section>
 			<footer>
