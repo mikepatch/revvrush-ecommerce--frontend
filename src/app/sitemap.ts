4eeef50cf6d6783f3fb0692b142afdd6433a0d07
Michal Lata <michallata.dev@gmail.com>
2024-04-03 11:44:10 +0200

    patch: Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,50 @@
 import { type MetadataRoute } from "next";
 
 export default function Sitemap(): MetadataRoute.Sitemap {
+	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+	if (!baseUrl) {
+		throw new Error("NEXT_PUBLIC_BASE_URL is not set");
+	}
+
 	return [
 		{
-			url: "http://localhost:3000",
+			url: baseUrl,
 			lastModified: new Date(),
 		},
 		{
-			url: "http://localhost:3000/collections",
+			url: `${baseUrl}/product/[productId]`,
 			lastModified: new Date(),
 		},
 		{
-			url: "http://localhost:3000/products",
+			url: `${baseUrl}/products`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/collections`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/collections/[collectionId]`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/cart`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/categories/[pageNumber]`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/categories/[categoryId]/[pageNumber]`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/static/privacy-policy`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/static/tems-of-service`,
 			lastModified: new Date(),
 		},
 	];
