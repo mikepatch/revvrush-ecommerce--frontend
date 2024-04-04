8e652502d1f853cba60f2aa7e779cf4edadd5c2e
Michal Lata <michallata.dev@gmail.com>
2024-04-04 09:47:58 +0200

    patch: Update CartTableRow

--- src/ui/molecules/Cart/CartTableRow.tsx
+++ src/ui/molecules/Cart/CartTableRow.tsx
@@ -13,18 +13,22 @@ type CartTableRowProps = {
 export const CartTableRow = ({ row }: CartTableRowProps) => {
 	return (
 		<tr className="shadow-sm transition-colors hover:bg-gray-50">
-			<td className="group w-1/2 px-4 py-6 text-left">
+			<th scope="row" className="group w-1/2 px-4 py-6 text-left">
 				<Link href={`/product/${row.productId}`} className="flex items-center gap-2">
 					<NextImage alt={row.name} src={row.image} width={50} height={50} />
 					<span className="transition-colors group-hover:text-brand-primary">{row.name}</span>
 				</Link>
-			</td>
-			<td className="px-4 py-6 text-left">
+			</th>
+			<td scope="row" className="px-4 py-6 text-left">
 				<CartItemQuantityForm itemId={row.cartItemId} quantity={row.quantity} />
 			</td>
-			<td className="px-4 py-6 text-left">{formatPrice(row.price)}</td>
-			<td className="px-4 py-6 text-left">{formatPrice(row.total)}</td>
-			<td className="px-2 py-6 text-center">
+			<td scope="row" className="px-4 py-6 text-left">
+				{formatPrice(row.price)}
+			</td>
+			<td scope="row" className="px-4 py-6 text-left">
+				{formatPrice(row.total)}
+			</td>
+			<td scope="row" className="px-2 py-6 text-center">
 				<RemoveFromCartButton itemId={row.cartItemId} />
 			</td>
 		</tr>
