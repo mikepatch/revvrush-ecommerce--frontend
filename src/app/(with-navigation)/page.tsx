8dfc1a1e61f2e252e93cee686b28c8a18c8d8b97
Michal Lata <michallata.dev@gmail.com>
2024-04-05 19:20:41 +0200

    patch: Update HomePage

--- src/app/(with-navigation)/page.tsx
+++ src/app/(with-navigation)/page.tsx
@@ -1,20 +1,42 @@
-import { getAllProducts } from "@/api/products";
-import { PageTitle } from "@/ui/atoms/PageTitle";
-import { ProductList } from "@/ui/molecules/List/ProductList";
-// import { CollectionsBanner } from "@/ui/organisms/CollectionsBannerCarousel";
+import { type Route } from "next";
+
+import { getAllCategories, getAllProducts } from "@/api/products";
 import { getAllCollections } from "@/api/collections";
-import { CollectionList } from "@/ui/molecules/List/CollectionList";
+import { Heading } from "@/ui/atoms/Heading";
+import { BannerSlider } from "@/ui/molecules/BannerSlider";
+import { type BannerSliderItemType } from "@/types";
+import { CategoriesList } from "@/ui/molecules/List/CategoriesList";
+import { CarouselProductList } from "@/ui/molecules/List/CarouselProductList";
 
 export default async function HomePage() {
-	const products = await getAllProducts({ take: 4 });
+	const recommendedProducts = await getAllProducts({
+		take: 4,
+		where: { avgRating: { equals: 5 } },
+	});
+	const categories = await getAllCategories();
 	const collections = await getAllCollections();
+	const bannerItems = collections.map(
+		({ coverImage, name, slug }): BannerSliderItemType => ({
+			img: coverImage,
+			name,
+			href: `/collections/${slug}` as Route,
+		}),
+	);
 
 	return (
-		<section className="flex flex-col gap-4">
-			{/* <CollectionsBanner collections={collections} /> */}
-			<CollectionList collections={collections} />
-			<PageTitle title="RevvRush" />
-			<ProductList products={products.data} />
-		</section>
+		<>
+			<section className="flex flex-col gap-8">
+				<div>
+					<BannerSlider items={bannerItems} />
+				</div>
+				<div className="flex flex-col gap-4">
+					<Heading level={2}>Shop by Category</Heading>
+					<CategoriesList categories={categories} />
+				</div>
+				<div>
+					<CarouselProductList items={recommendedProducts.data} title="Recommended Products" />
+				</div>
+			</section>
+		</>
 	);
 }
