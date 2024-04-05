5710a3e895ad136ac340f66ec7883860cc4c90cc
Michal Lata <michallata.dev@gmail.com>
2024-04-05 19:26:07 +0200

    patch: Update Header

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -1,16 +1,20 @@
+import { getAllCategories } from "@/api/products";
 import { type NavItemType } from "@/types";
 import { HeaderBottomNavbar } from "@/ui/molecules/Navigation/HeaderBottomNavbar";
 import { HeaderTopNavbar } from "@/ui/molecules/Navigation/HeaderTopNavbar";
 
-type HeaderProps = {
-	categories: NavItemType[];
-};
+export const Header = async () => {
+	const { data: productCategories } = await getAllCategories();
+	const categoriesItems = productCategories.map((category) => ({
+		id: category.id,
+		href: `/categories/${category.slug}`,
+		label: category.name,
+	})) as NavItemType[];
 
-export const Header = ({ categories }: HeaderProps) => {
 	return (
 		<header className="sticky left-0 right-0 top-0 z-20 flex h-header-height flex-col items-center justify-between bg-brand-background-dark bg-gradient-to-b from-[#2d2d2d] text-font-light shadow-sm">
 			<HeaderTopNavbar />
-			<HeaderBottomNavbar categories={categories} />
+			<HeaderBottomNavbar categories={categoriesItems} />
 		</header>
 	);
 };
