ff3d00772c1d79db73121aa19a00fef82fad85fd
Michal Lata <michallata.dev@gmail.com>
2024-04-06 11:40:32 +0200

    patch: Refactor HomePage

--- src/app/(with-navigation)/page.tsx
+++ src/app/(with-navigation)/page.tsx
@@ -2,7 +2,6 @@ import { type Route } from "next";
 
 import { getAllCategories, getAllProducts } from "@/api/products";
 import { getAllCollections } from "@/api/collections";
-import { Heading } from "@/ui/atoms/Heading";
 import { BannerSlider } from "@/ui/molecules/BannerSlider";
 import { type BannerSliderItemType } from "@/types";
 import { CategoriesList } from "@/ui/molecules/List/CategoriesList";
@@ -26,16 +25,9 @@ export default async function HomePage() {
 	return (
 		<>
 			<section className="flex flex-col gap-8">
-				<div>
-					<BannerSlider items={bannerItems} />
-				</div>
-				<div className="flex flex-col gap-4">
-					<Heading level={2}>Shop by Category</Heading>
-					<CategoriesList categories={categories} />
-				</div>
-				<div>
-					<CarouselProductList items={recommendedProducts.data} title="Recommended Products" />
-				</div>
+				<BannerSlider items={bannerItems} />
+				<CategoriesList categories={categories} title="Shop by Category" />
+				<CarouselProductList items={recommendedProducts.data} title="Recommended Products" />
 			</section>
 		</>
 	);
