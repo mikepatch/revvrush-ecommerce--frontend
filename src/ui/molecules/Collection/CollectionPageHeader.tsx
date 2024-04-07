35c67d0c3d78696022cca762b45edfd7e5e415c3
Michal Lata <michallata.dev@gmail.com>
2024-04-07 18:30:15 +0200

    feat: Add CollectionPageHeader

---
+++ src/ui/molecules/Collection/CollectionPageHeader.tsx
@@ -0,0 +1,29 @@
+import NextImage from "next/image";
+import { type CollectionWithProductsFragment } from "@/gql/graphql";
+import { Heading } from "@/ui/atoms/Heading";
+
+type CollectionPageHeaderProps = {
+	collection: CollectionWithProductsFragment;
+};
+
+export const CollectionPageHeader = ({ collection }: CollectionPageHeaderProps) => {
+	return (
+		<header className="flex flex-col gap-2 bg-brand-background-lighter pb-4">
+			<figure className="relative">
+				<NextImage
+					width={1440}
+					height={400}
+					src={`/images/collections/${collection.slug}.png`}
+					alt={collection.name}
+				/>
+				<Heading
+					level={1}
+					className="absolute inset-0 flex items-center justify-center bg-brand-background-dark bg-opacity-90 p-4 text-6xl text-font-light"
+				>
+					{collection.name}
+				</Heading>
+			</figure>
+			<p className="p-4">{collection.description}</p>
+		</header>
+	);
+};
