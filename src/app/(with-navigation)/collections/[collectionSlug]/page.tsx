f401f1accd0dedc48ff25e16abf46f956505e4bd
Michal Lata <michallata.dev@gmail.com>
2024-04-07 18:30:08 +0200

    patch: Refactor SingleCollectionPage

--- src/app/(with-navigation)/collections/[collectionSlug]/page.tsx
+++ src/app/(with-navigation)/collections/[collectionSlug]/page.tsx
@@ -1,9 +1,8 @@
 import { type Metadata } from "next";
-import NextImage from "next/image";
 
 import { getCollectionBySlug } from "@/api/collections";
 import { CollectionProductList } from "@/ui/molecules/List/CollectionProductList";
-import { Heading } from "@/ui/atoms/Heading";
+import { CollectionPageHeader } from "@/ui/molecules/Collection/CollectionPageHeader";
 
 type ParamsType = {
 	collectionSlug: string;
@@ -23,30 +22,13 @@ type SingleCollectionPageProps = {
 
 export default async function SingleCollectionPage({ params }: SingleCollectionPageProps) {
 	const { collectionSlug } = params;
-	const { name, description, products } = await getCollectionBySlug(collectionSlug);
+	const collection = await getCollectionBySlug(collectionSlug);
 
 	return (
 		<>
-			<header className="flex flex-col gap-2 bg-brand-background-lighter pb-4">
-				<figure className="relative">
-					<NextImage
-						width={1440}
-						height={400}
-						src={`/images/collections/${collectionSlug}.png`}
-						alt={name}
-					/>
-					<Heading
-						level={1}
-						className="absolute inset-0 flex items-center justify-center bg-brand-background-dark bg-opacity-90 p-4 text-6xl text-font-light"
-					>
-						{name}
-					</Heading>
-				</figure>
-				<p className="p-4">{description}</p>
-			</header>
-			{/* <hr className="border-brand-primary" /> */}
+			<CollectionPageHeader collection={collection} />
 			<section>
-				<CollectionProductList products={products} />
+				<CollectionProductList products={collection.products} />
 			</section>
 		</>
 	);
