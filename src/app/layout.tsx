abc0f1caf493cbb80bf64b5e21e35ba385218236
Michal Lata <michallata.dev@gmail.com>
2024-06-22 18:31:37 +0200

    feat: Implement clerk at the top of the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 
 import "../styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Footer } from "@/ui/organisms/Footer";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
@@ -24,14 +25,16 @@ export const metadata: Metadata = {
 };
 export default function RootLayout({ children, modal }: RootLayoutProps) {
 	return (
-		<html lang="en">
-			<body
-				className={`${rubik.className} min-h-screen overflow-x-hidden bg-brand-background text-font-dark antialiased`}
-			>
-				{children}
-				<Footer />
-				{modal}
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body
+					className={`${rubik.className} min-h-screen overflow-x-hidden bg-brand-background text-font-dark antialiased`}
+				>
+					{children}
+					<Footer />
+					{modal}
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
