5f716996175fdd12e19a0ad472b150d3aadc1760
Michal Lata <michallata.dev@gmail.com>
2024-08-24 17:52:09 +0200

    patch: Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/categories/wheels/(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/account"]);
 
 export default clerkMiddleware((auth, req) => {
 	if (isProtectedRoute(req)) auth().protect();
