4d44c2a62a882a91a275373ed500548f8a43f926
Michal Lata <michallata.dev@gmail.com>
2024-12-10 21:46:48 +0100

    patch: Update modal with using force-dynamic

--- src/app/@modal/(.)cart/page.tsx
+++ src/app/@modal/(.)cart/page.tsx
@@ -1,6 +1,8 @@
 import { Overlay } from "@/ui/atoms/Overlay";
 import { CartModal } from "@/ui/molecules/Cart/CartModal";
 
+export const dynamic = "force-dynamic";
+
 export default function ModalCart() {
 	return (
 		<>
